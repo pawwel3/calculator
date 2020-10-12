35889933ad361e513d3c95532ff4173ef0758a7f
Paweł Dobrowolski <pawwel3@gmail.com>
2020-10-12 14:42:12 +0200

    Updates

---
+++ precache-manifest.bd38117052fa49aedbc0fd3ce6684b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4593d742d5d17b671369b52cd3da15",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "954fad1f0b9963496775",
+    "url": "/calculator/static/css/main.d119c885.chunk.css"
+  },
+  {
+    "revision": "ecada51e1a3334c42b99",
+    "url": "/calculator/static/js/2.e6f076d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.e6f076d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "954fad1f0b9963496775",
+    "url": "/calculator/static/js/main.25b47341.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
